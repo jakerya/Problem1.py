21b383f9130b6557aab2f2480c6bce8df206cac3
Jakerya Patton <jakeryapatton@Jakeryas-Air.lan>
2023-11-03 21:52:34 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Collecting length input from the user
+length = float(input("Enter the length of the rectangle: "))
+
+# Collecting breadth input from the user
+breadth = float(input("Enter the breadth of the rectangle: "))
+
+# Calculating the area of the rectangle
+area = length * breadth
+
+# Printing the result
+print(f"The area of the rectangle is: {area}")
